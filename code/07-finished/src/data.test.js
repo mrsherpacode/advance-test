a75f4b23f66691e0f25d6576219991491e80517d
mrsherpacode <sherpacoding@gmail.com>
2026-05-13 20:30:58 -0500

    code-2 folder added

--- code/07-finished/src/data.test.js
+++ code/07-finished/src/data.test.js
@@ -1,9 +1,9 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect, vi } from "vitest";
 
-import { generateReportData } from './data';
+import { generateReportData } from "./data";
 
-describe('generateReportData()', () => {
-  it('should execute logFn if provided', () => {
+describe("generateReportData()", () => {
+  it("should execute logFn if provided", () => {
     const logger = vi.fn();
 
     // logger.mockImplementationOnce(() => {});
